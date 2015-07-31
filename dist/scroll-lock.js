0099468e7c01833efa3edff0b80483eadce107ff
Daniel Wolf <daniel.wolf@punchkickinteractive.com>
2015-07-30 21:31:53 -0500

    code cleanup, fixed small bug, comments everywhere, ready for launch!

--- dist/scroll-lock.js
+++ dist/scroll-lock.js
@@ -1,36 +1,79 @@
 define([], function () {
     var ScrollLock = function () {
-        var disabled = false, currentScrollPosition, previousOverflow,
+
+        /**
+         * Whether the window is currently locked
+         * @type {boolean}
+         */
+        var locked = false,
+
+            /**
+             * X and Y scroll position of the window
+             * @type {array}
+             */
+            scrollPosition,
+
+            /**
+             * Current overflow property value
+             */
+            overflow,
+
+            /**
+             * touchmove callback,
+             * disables default action of event
+             * @param e
+             */
             touchMoveCallback = function (e) {
                 e.preventDefault();
             };
 
+        /**
+         * Lock the scroll position and disable scrolling
+         */
         this.lock = function () {
-            if (disabled === true) {
+            // make sure it isn't already disabled
+            if (locked === true) {
                 return;
             }
-            disabled = true;
+            locked = true;
 
-            currentScrollPosition = [
+            // save the current scroll position (x and y)
+            scrollPosition = [
                 self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
                 self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
             ];
-            previousOverflow = document.body.style.overflow;
-            document.body.style.overflow = 'hidden';
-            window.scrollTo(currentScrollPosition[0], currentScrollPosition[1]);
+            // save the current overflow
+            overflow = document.body.style.overflow;
 
+            // set the overflow to hidden,
+            // which will snap the window to the top
+            document.body.style.overflow = 'hidden';
+            // scroll the window back down to the current scroll position
+            window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
+            // FOR MOBILE
+            // add a touchmove event listener to disable this action
             document.addEventListener('touchmove', touchMoveCallback);
         };
 
+        /**
+         * Unlock the scroll position and enable scrolling (if disabled)
+         */
         this.unlock = function () {
-            if (disabled === false) {
+            // make sure it is already disabled
+            if (locked === false) {
                 return;
             }
-            disabled = false;
+            locked = false;
 
-            document.body.style.overflow = previousOverflow;
-            window.scrollTo(currentScrollPosition[0], currentScrollPosition[1]);
+            // set the overflow back to whatever it was,
+            // which may snap the window in undesired ways
+            document.body.style.overflow = overflow;
+            // scroll the window back to the original scroll position
+            window.scrollTo(scrollPosition[0], scrollPosition[1]);
 
+            // FOR MOBILE
+            // remove the event listener that disables the touchmove action
             document.removeEventListener('touchmove', touchMoveCallback);
         };
     };
